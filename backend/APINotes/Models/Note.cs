0673f888d53377b294acaf519613c4b059cc0ab3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2024-11-25 16:11:05 -0300

    feature: models for database

---
+++ backend/APINotes/Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace APINotes.Models
+{
+    public class Note
+    {
+        [Key]
+        // Primary key
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "{0} value cannot exceed {1} characters.")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(250, ErrorMessage = "{0} value cannot exceed {1} characters.")]
+        public string Content { get; set; } = null!;
+
+        // Categories
+        public List<string> Categories { get; set; } = new();
+
+        // Handle logic delete
+        public bool isActive { get; set; }
+
+        // Foreign key
+        public Guid UserId { get; set; }
+
+        // Navigation property
+        public User User { get; set; } = null!;
+    }
+}
---
+++ backend/APINotes/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APINotes.Models
+{
+    public class User
+    {
+        // Primary key
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "{0} value cannot exceed {1} characters.")]
+        public string Username { get; set; } = null!;
+
+        [RegularExpression(@"^^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[\S]{8,}$",
+        ErrorMessage = "{0} must contain at least one special character, one number, one upper case and one lower case.")]
+        [StringLength(100, ErrorMessage = "{0} value cannot exceed {1} characters.")]
+        public string Password { get; set; } = null!;
+
+        // Notes created
+        public List<Note> NotesCreated { get; set; } = new();
+
+        // Notes archived
+        public List<Note> NotesArchived { get; set; } = new();
+
+        // Logic delete
+        public bool IsActive { get; set; }
+
+
+    }
+}
