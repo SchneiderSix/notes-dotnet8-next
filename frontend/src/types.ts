6143aaeecac5d8b5ed6d28df6aba733b1f1faa6d
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2024-11-28 03:39:23 -0300

    fix: interfaces

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,13 +1,15 @@
-export interface NoteAPIResponse {
-  notes: {
-    $values: Array<{
-      noteId: string,
+export interface Note {
+  noteId: string,
       userId: string,
       title: string,
       content: string,
       tags: {
         $values: string[]
       }
-    }>
+}
+
+export interface NoteAPIResponse {
+  notes: {
+    $values: Note[]
   }
 }
